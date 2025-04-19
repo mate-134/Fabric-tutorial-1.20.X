f12fa579268eac3877f8d89288c93deb13871474
Matija <matijafr13@gmail.com>
2025-04-19 20:26:17 +0200

    Initial commit

---
+++ src/main/java/net/matija/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.matija.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
